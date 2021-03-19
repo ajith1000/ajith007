9d6a3d70c1c394c2bc845bc5cf565702bf5f7861
viddhu <viddhu91@gmail.com>
2021-03-19 18:17:34 +0530

    verified

--- src/org/test/Employee.java
+++ src/org/test/Employee.java
@@ -4,4 +4,12 @@ public class Employee {
 private void method1() {
 System.out.println("qyui");
 }
+private void hai() {
+	// TODO Auto-generated method stub
+
+}
+private void hello() {
+	// TODO Auto-generated method stub
+
+}
 }
